c19d970874bae9e4d30a4de3b30d1c04d66772e4
liuwei <orcsor@qq.com>
2019-11-28 12:24:00 +0800

    修改启动文件

---
+++ swiftlearning/swiftlearning/Base/LWNavigationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LWNavigationViewController.swift
+//  swiftlearning
+//
+//  Created by liuwei on 2019/11/28.
+//  Copyright © 2019 liuwei. All rights reserved.
+//
+
+import UIKit
+
+class LWNavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
